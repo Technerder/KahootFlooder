9bbd09462c21f3b01be65a0023bdb7005d4988f6
Technerder <technerder@hotmail.com>
2018-08-30 15:40:18 -0400

    Reduced ram usage by ~30%

--- KahootFlooder.py
+++ KahootFlooder.py
@@ -1,17 +1,25 @@
 from time import sleep
 from threading import Thread
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-numberOfBots = 1
+numberOfBots = 25
 baseName = 'Bot'
-gamePin = ''
+gamePin = '635746'
 
 delayBeforeLeaving = 20
 delayBetweenActions = 2.5
 
 def createBot(pin, name):
+
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("test-type")
+    options.add_argument("--js-flags=--expose-gc")
+    options.add_argument("--enable-precise-memory-info")
+    options.add_argument("--disable-default-apps");
     
-    browser = webdriver.PhantomJS()
+    browser = webdriver.Chrome(chrome_options = options)
     browser.get("http://kahoot.it")
     
     sleep(delayBetweenActions)
@@ -23,10 +31,10 @@ def createBot(pin, name):
     
     browser.find_element_by_css_selector('#username').send_keys((u'\u200b').join(name))
     browser.find_element_by_css_selector('.btn-greyscale').click()
-    
+
     sleep(delayBeforeLeaving)
 
-    browser.close()
+    #browser.close()
     
 for i in range(numberOfBots): 
     
